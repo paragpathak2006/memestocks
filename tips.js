ae7964190704b132a3b8a48efe9f7d785c6d3e95
paragpathak2006 <31978917+paragpathak2006@users.noreply.github.com>
2021-03-31 22:20:00 -0500

    Create tips.js

---
+++ tips.js
@@ -0,0 +1,5 @@
+window.alert("Tips:\n\
+- Press OK to open the world.\n\
+- If opening fails, upgrade to latest version of Safari/Chrome\n\
+  and exit private/incognito mode.\n\
+- Visit https://www.spotmaze.com for Login access or Faqs ");

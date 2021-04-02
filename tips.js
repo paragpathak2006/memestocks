4b29c91866eee11c9b8474100d9a4c906ddb7850
paragpathak2006 <31978917+paragpathak2006@users.noreply.github.com>
2021-04-02 17:35:38 -0500

    Update tips.js

--- tips.js
+++ tips.js
@@ -1,5 +1,5 @@
 window.alert("Tips:\n\
 - Press OK to open the world.\n\
-- If opening fails, upgrade to latest version of Safari/Chrome\n\
+- For best viewing, upgrade to latest version of Safari/Chrome\n\
   and exit private/incognito mode.\n\
-- Visit https://www.spotmaze.com for Login access or Faqs ");
+- Visit https://www.spotmaze.com for Login access or Faqs");
